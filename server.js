812a91763d2f333ba6463b18323e270253dd84a2
vishwas1 <vishwasanand.cse@gmail.com>
2020-03-30 22:41:56 +0530

    Addde swagger to the template

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const express = require('express'),
   port = process.env.PORT || 3000;
   bodyParser = require('body-parser');
 
+
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-  
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
+
 
 const routes = require('./api/routes/walletRoute')
 routes(app);
