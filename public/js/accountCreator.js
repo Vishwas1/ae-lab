b0b1c8069a369d4f2cc0558629f65bd0cb62d65a
vikramide <>
2020-03-31 00:50:11 +0530

    Added code for account and transaction

--- public/js/accountCreator.js
+++ public/js/accountCreator.js
@@ -1,1 +1,9 @@
-console.log('Account Creator')
+
+function generateKeyPair(params) {
+    console.log('Account Creator')
+    let pubKey = document.getElementById('publicKey')
+    pubKey.textContent = "GCR7TGFGWWMRFQACXFNX7XQ2NNQRA3E7JSBZ44W6KKNOOAZLM7V3N2ZW"
+
+    let secretKey = document.getElementById('secretKey')
+    secretKey.textContent = "SDAZYYZ7DBGCPVUZ46OHF3O2YL3FNK3HGXJQNFYPHT5FBE23Z55Z4JOK"
+}
