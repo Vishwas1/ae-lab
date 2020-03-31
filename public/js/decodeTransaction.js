ac1dedc2df1c4ae5a3e857a4eb128cd296ca1e47
vishwas1 <vishwasanand.cse@gmail.com>
2020-03-31 11:16:20 +0530

    decode tx api indetrated

--- public/js/decodeTransaction.js
+++ public/js/decodeTransaction.js
@@ -1,1 +1,22 @@
-console.log('decode TX')
+async function decodeTx() {
+  console.log('Decode TX')
+  const url = `${host}/decode`;
+  
+  const body = {
+    tx: $('#decode_signedTx').val(),
+  }
+
+  const resp =  await fetch(url, {
+      method : 'post',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body : JSON.stringify(body)
+  })
+  const json = await resp.json();
+
+  let rawTx = document.getElementById('decode_parsedTx')
+  console.log(json.parsedTx.tx)
+  rawTx.textContent = JSON.stringify(json.parsedTx.tx);
+}
