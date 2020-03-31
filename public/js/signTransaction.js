b4f6884f1aaf08dd2b9a3a9e03d1be3477c18f9d
vishwas1 <vishwasanand.cse@gmail.com>
2020-03-31 10:55:02 +0530

    sign tx api integration

--- public/js/signTransaction.js
+++ public/js/signTransaction.js
@@ -1,1 +1,23 @@
-console.log('Sign TX')
+async function signTx() {
+  console.log('Sign TX')
+  const url = `${host}/sign`;
+  
+  const body = {
+      secretKey: $('#sign_privateKey').val(),
+      publicKey: $('#sign_publicKey').val(),
+      rawTx: $('#sign_rawTx').val()
+  }
+
+  const resp =  await fetch(url, {
+      method : 'post',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body : JSON.stringify(body)
+  })
+  const json = await resp.json();
+
+  let rawTx = document.getElementById('sign_signedTx')
+  rawTx.textContent = json.signTx;
+}
