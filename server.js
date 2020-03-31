43bd5cfac3c200db0ae3281e10c69e57b41a7cc0
vishwas1 <vishwasanand.cse@gmail.com>
2020-03-31 09:23:32 +0530

    updated swagger and postman file

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express'),
   bodyParser = require('body-parser');
 
 
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
