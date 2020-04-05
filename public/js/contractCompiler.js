0637830fae976cbba7afdc35d3f58620349bc682
vikramide <>
2020-04-06 01:46:18 +0530

    Added the design for the compile and deploy and Call Function

---
+++ public/js/contractCompiler.js
@@ -0,0 +1,18 @@
+async function compileContract() {
+    
+    let pvtKey = document.getElementById('modal_sign_privateKey')
+    let pubKey = document.getElementById('modal_sign_publicKey')
+
+    /**
+     * Rest of the code goes here
+     */
+    console.log(pvtKey);
+    console.log(pubKey);
+
+    $(".callFunction").show();
+}
+
+
+
+
+
