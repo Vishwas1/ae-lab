f9ac19a5fccf76c744805e64089a1707cb5d68b3
vikramide <>
2020-05-06 20:10:52 +0530

    keycloak-integration

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,54 +1,69 @@
-{/* <script src="http://192.168.1.5:8080/auth/js/keycloak.js"></script> */}
-    /* If the keycloak.json file is in a different location you can specify it:
 
-    Try adding file to application first, if you fail try the another method mentioned below. Both works perfectly.
+    const keycloak = new Keycloak('http://192.168.1.2:5000/keycloak.json');
 
-    var keycloak = Keycloak('http://localhost:8080/myapp/keycloak.json'); */
-
-    /* Else you can declare constructor manually  */
-
-    // var keycloak = Keycloak({
-    //     url: 'http://192.168.1.5:8080/auth',
-    //     realm: 'master',
-    //     clientId: 'hs-playground-local'
-    // });
-
-
-    // keycloak.init({onLoad: 'check-sso' })
-    // .then(function(authenticated) {
-    //     alert(authenticated ? 'authenticated' : 'not authenticated');
-    // }).catch(function() {
-    //     alert('failed to initialize');
-    // });
-
-    // function logout() {
-    //     keycloak.logout('http://192.168.1.5:8080/auth/realms/Internal_Projects/protocol/openid-connect/logout?redirect_uri=encodedRedirectUri')
-    //     //alert("Logged Out");
-    // }
-
-const checkAuth = (cname) => {
-
-    let cvalue = getCookie(cname);
-    console.log(`cookie value for ${cname} is ${cvalue}`)
-    if (cvalue != "") {
-        console.log(`Setting value for ${cname} is ${cvalue} in textbox`)
-        return cvalue
-    } else {
-        return false
+    const logout = () => {
+        let logoutUrl = keycloak.createLogoutUrl()
+        keycloak.logout(logoutUrl)
     }
-}
-$(document).ready(function () {
 
-    if (checkAuth('token'))
-    {
-        alert('Cookie is set')
-        $('.auth--check').show()
-        $('.auth--uncheck').hide()
-    } else {
-        let pubKey = formatBech32('one1m6mucnf577j2uecark5swpeexz79st8jgl77yy',false,8,8)
+    const login = () => {
+        keycloak.login()
+    }
+
+    const setUserProfile = () => {
+        //Setting data to be displayed on the side var
+
+        //PubKey
+        $('.formatBech32').text('')
+        let pubKey = formatBech32(keycloak.subject, false,4,4)
+        $('.formatBech32').text(pubKey)
+
+        //UserName  
+        let userName = keycloak.tokenParsed.preferred_username
+        $('.userName').text(userName)
+
+        //Email
+        $('.userEmail').text(keycloak.tokenParsed.email)
+
+        //User Initial
+        $('.userInitial').text(userName.substring(0, 2).toUpperCase())
+
+    }
+
+    $(document).ready(function () {
         
-        $('.formatBech32').text( pubKey)
-        $('.auth--check').hide()
-        $('.auth--uncheck').show()
-    }
-});
+        //If user is looged 
+        // Chec if someone is loogedin the session
+        // Then dont do anything keep in the same browser
+
+        //if user is not logged
+        
+        keycloak.init({ onLoad: 'check-sso' }).then(function (authenticated) {
+            debugger
+            if (authenticated) {
+                setUserProfile()
+                $('.auth--check').show()
+                $('.auth--uncheck').hide()
+            } else {
+                $('.auth--check').hide()
+                $('.auth--uncheck').show()
+            }
+        }).catch(function () {
+            debugger
+            alert('failed to initialize');
+        });
+        // if (checkAuth('token'))
+        // {
+        //     alert('Cookie is set')
+        //     $('.auth--check').show()
+        //     $('.auth--uncheck').hide()
+        // } else {
+        //     let pubKey = formatBech32('one1m6mucnf577j2uecark5swpeexz79st8jgl77yy',false,8,8)
+
+        //     $('.formatBech32').text( pubKey)
+        //     $('.auth--check').hide()
+        //     $('.auth--uncheck').show()
+        // }
+    });
+
+
